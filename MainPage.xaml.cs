2c865b20128b90e7987b8821b9e8f106f1f977ab
Krzysztof Blicharz <19705@student.ans-elblag.pl>
2024-01-09 11:39:12 +0100

    UI & Basic game logic

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,76 @@
-﻿namespace Hangman
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Hangman
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
-        int count = 0;
+        private string spotlight;
+
+        #region UI Properties
+
+        public string Spotlight
+        {
+            get => spotlight;
+            set
+            {
+                spotlight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion UI Properties
+
+        #region Fields
+
+        List<string> words = new List<string>()
+        {
+            "python",
+            "java",
+            "kotlin",
+            "swift",
+            "javascript",
+            "php",
+            "html",
+            "css",
+            "csharp",
+            "ruby",
+            "sql",
+            "perl",
+            "word",
+            "excel",
+            "powerpoint",
+            "access",
+            "outlook",
+        };
+
+        string answer = "";
+        List<char> guessed = new List<char>();
+
+        #endregion Fields
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            PickWord();
+            CalculateWord(answer, guessed);
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        #region Game Engine
+
+        private void PickWord()
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            answer = words[new Random().Next(0, words.Count)];
+            Debug.WriteLine($"Answer is {answer}");
         }
-    }
 
+        private void CalculateWord(string answer, List<char> guessed)
+        {
+            var temp = answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_')).ToArray();
+            Spotlight = string.Join(' ', temp);
+        }
+
+        #endregion Game Engine
+    }
 }
