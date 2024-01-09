2bbf5c9e0f1c78fef4a858b60ad769652346026e
Krakerex <58233618+Krakerex@users.noreply.github.com>
2024-01-09 16:57:30 +0100

    Finished game logic

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,7 +6,7 @@ namespace Hangman
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
         private string spotlight;
-
+        private List<char> letters = new List<char>();
         #region UI Properties
 
         public string Spotlight
@@ -18,7 +18,39 @@ namespace Hangman
                 OnPropertyChanged();
             }
         }
+        public List<char> Letters
+        {
+            get => letters; set
+            {
+                letters = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Message
+        {
+            get => message; set
+            {
 
+                message = value;
+                OnPropertyChanged();
+            }
+        }
+        public string GameStatus
+        {
+            get => gameStatus; set
+            {
+                gameStatus = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CurrentImage
+        {
+            get => currentImage; set
+            {
+                currentImage = value;
+                OnPropertyChanged();
+            }
+        }
         #endregion UI Properties
 
         #region Fields
@@ -46,12 +78,18 @@ namespace Hangman
 
         string answer = "";
         List<char> guessed = new List<char>();
+        private string message;
+        int mistakes= 0;
+        int maxwrong = 6;
+        private string gameStatus;
+        private string currentImage="img0.jpg";
 
         #endregion Fields
 
         public MainPage()
         {
             InitializeComponent();
+            Letters.AddRange("abcdefghijklmnopqrstuvwxyz");
             BindingContext = this;
             PickWord();
             CalculateWord(answer, guessed);
@@ -72,5 +110,95 @@ namespace Hangman
         }
 
         #endregion Game Engine
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                var letter = btn.Text;
+                btn.IsEnabled = false;
+                HandleGuess(letter[0]);
+            }
+        }
+
+        private void HandleGuess(char letter)
+        {
+            if (guessed.IndexOf(letter) == -1)
+            {
+                guessed.Add(letter);
+            }
+            if(answer.IndexOf(letter) >= 0)
+            {
+                CalculateWord(answer, guessed);
+                CheckIfGameWon();
+            }
+            else if(answer.IndexOf(letter) == -1)
+            {
+                mistakes++;
+                UpdateStatus();
+                CheckIfGameLost();
+                CurrentImage = $"img{mistakes}.jpg";
+            }
+        }
+
+        private void CheckIfGameLost()
+        {
+            if(mistakes == maxwrong)
+            {
+                Message = "You lost!";
+                DisableLetters();
+            }
+        }
+
+        private void DisableLetters()
+        {
+            foreach(var child in LettersContainer.Children)
+            {
+                var btn = child as Button;
+                if(btn != null)
+                {
+                    btn.IsEnabled = false;
+                }
+            }
+        }
+
+        private void CheckIfGameWon()
+        {
+            if(Spotlight.Replace(" ", "") == answer)
+            {
+                Message= "You won!";
+                DisableLetters();
+            }
+        }
+        private void UpdateStatus()
+        {
+
+                GameStatus = $"Errors: {mistakes}/{maxwrong}.";
+        }
+
+        private void Reset_Clicked(object sender, EventArgs e)
+        {
+            mistakes = 0;
+            guessed = new List<char>();
+            CurrentImage = "img0.jpg";
+            PickWord();
+            CalculateWord(answer, guessed);
+            Message = "";
+            UpdateStatus();
+            EnableLetters();
+        }
+
+        private void EnableLetters()
+        {
+            foreach (var child in LettersContainer.Children)
+            {
+                var btn = child as Button;
+                if (btn != null)
+                {
+                    btn.IsEnabled = true;
+                }
+            }
+        }
     }
 }
